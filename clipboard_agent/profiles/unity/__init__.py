4fcd658ca3d8cb52978a50df6e59841122b0df88
Matthis-Dubos <119478794+JeanTaniban@users.noreply.github.com>
2026-09-23 22:32:38 +0200

    Add Unity profile package

---
+++ clipboard_agent/profiles/unity/__init__.py
@@ -0,0 +1,12 @@
+from .profile import UnityProfile
+from .state import UnityProfileState
+from .visual import EvidenceBundle, UnityVisualRouter, VisualConfidence, VisualIntent
+
+__all__ = [
+    "EvidenceBundle",
+    "UnityProfile",
+    "UnityProfileState",
+    "UnityVisualRouter",
+    "VisualConfidence",
+    "VisualIntent",
+]
